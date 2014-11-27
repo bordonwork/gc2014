cd286e407ffd1c35d3b09d71109242b56a3d865f
Roque Ocampo <roko.okmpo@gmail.com>
2014-11-27 20:38:55 -0300

    Clase Plane

--- src/plane.h
+++ src/plane.h
@@ -1,7 +1,26 @@
+#ifndef _PLANE_H_
+#define _PLANE_H_
+
 #include "Object3D.h"
-class Plane: public Object3D{
+
+class Plane: public Object3D {
 
 public:
-	Plane(Vec3f &normal, float d, Vec3f &color);
-	bool intersect(const Ray &,Hit &,float);
+	//Constructores
+	Plane() {}
+	Plane( Vec3f& normal, float d, Vec3f& color);
+
+	//Destructor
+	~Plane() {}
+
+	virtual bool intersect(Ray& r , Hit& h , float tmin)=0;
+
+protected:
+	//Vector normal
+	Vec3f _normal;
+	//distancia del origen
+	float _d;
+
 };
+
+#endif
